bf4a3aca49834d0df11aef274d0e45de755e075c
Aryan Singh <singharyan90827@gmail.com>
2026-03-16 14:33:45 +0530

    add deployment config

--- eda_service/app.py
+++ eda_service/app.py
@@ -834,6 +834,6 @@ def new_password():
     return redirect(url_for('home'))
 
 
-
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
